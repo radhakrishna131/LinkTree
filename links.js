bb1ec0d02299b4204aed79381680bcde90afbf03
Radha Krishna <radhakrishna628191@gmail.com>
2026-02-12 22:31:17 +0530

    Add files via upload

---
+++ links.js
@@ -0,0 +1,21 @@
+export const links=[{
+  name:"Instagram",
+  link:"https://www.instagram.com/radha_krishna.2810?igsh=NWs2Z3Z0Y3J5bjc2",
+  image:"/Images/instagram.png"
+},
+{
+  name: "Linkedin",
+  link: "https://www.linkedin.com/in/radhakrishnasure?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+  image: "/Images/linkedin.png"
+},
+{
+  name: "Must Material",
+  link: "https://mustmaterial.vercel.app/",
+  image: "/Images/mustmaterial.png"
+},
+{
+  name:"Github",
+  link:"https://github.com/radhakrishna131",
+  image:"/Images/github.png"
+}
+]
---
+++ main.js
@@ -0,0 +1,15 @@
+import {links} from './links.js';
+const linksContainer=document.querySelector('.linkcontainer');
+function addLink(name,link,image){
+  return `<ul class="linkcontainer">
+        <a href="${link}">
+          <li><span>${name}</span><span><img src="${image}" alt="${name}" class="icons"/></span></li></a>`
+}
+let allLinks="";
+links.forEach((ele)=>{
+  let link=ele.link;
+  let image=ele.image;
+  let name=ele.name
+  allLinks+=addLink(name,link,image);
+});
+linksContainer.innerHTML=allLinks;
